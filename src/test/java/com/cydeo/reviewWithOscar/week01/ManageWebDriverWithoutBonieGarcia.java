cf1d88c83e796f05ab90fc3f9fdbebe09a2c70a7
hrnoksz <harun1973@yahoo.com>
2022-05-05 05:48:44 +0300

    day2 Task1_CydeoVerifications

---
+++ src/test/java/com/cydeo/reviewWithOscar/week01/ManageWebDriverWithoutBonieGarcia.java
@@ -0,0 +1,15 @@
+package com.cydeo.reviewWithOscar.week01;
+
+public class ManageWebDriverWithoutBonieGarcia {
+
+    public static void main(String[] args) {
+
+        //without Bonie Garcia
+        /*
+        Get the driver which is suitable for your OS, and browser version
+        Then give the path to Java System settings
+         */
+
+
+    }
+}
