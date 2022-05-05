1341ef72cba29eb0e2ab23b61ea057bda73aaf6a
hrnoksz <harun1973@yahoo.com>
2022-05-05 12:44:31 +0300

    day2 Task4_LibraryLoginPage

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T4_LibraryLoginPage.java
@@ -0,0 +1,19 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+public class T4_LibraryLoginPage {
+
+    public static void main(String[] args) {
+
+        //TC #4: Library verifications
+        //1. Open Chrome browser
+        //2. Go to https://library2.cybertekschool.com/login.html
+        //3. Enter username: “incorrect@email.com”
+        //4. Enter password: “incorrect password”
+        //5. Click to Sign in button
+        //6. Verify: visually “Sorry, Wrong Email or Password”
+        //displayed
+
+
+
+    }
+}
