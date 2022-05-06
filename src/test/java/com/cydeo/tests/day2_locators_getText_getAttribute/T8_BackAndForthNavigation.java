d97b982c6a9b72c1a38bde035d1074edc4fa7bcc
hrnoksz <harun1973@yahoo.com>
2022-05-06 22:21:07 +0300

    day2 T8_BackAndForthNavigation

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T8_BackAndForthNavigation.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T8_BackAndForthNavigation.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T8_BackAndForthNavigation {
@@ -18,13 +20,31 @@ public class T8_BackAndForthNavigation {
         driver.get("https://google.com");
 
         //3- Click to Gmail from top right.
-
+        WebElement gmailTest = driver.findElement(By.linkText("Gmail"));
+        gmailTest.click();
 
         //4- Verify title contains:
         //Expected: Gmail
+        String expectedTitle = "Gmail";
+        String actualTitle = driver.getTitle();
+        if(actualTitle.contains(expectedTitle)){
+            System.out.println("Title verification passed");
+        }else{
+            System.out.println("Title verification failed");
+        }
+
         //5- Go back to Google by using the .back();
+        driver.navigate().back();
+
         //6- Verify title equals:
         //Expected: Google
+        String expectedTitle1 = "Google";
+        String actualTitle1 = driver.getTitle();
+        if(actualTitle1.equals(expectedTitle1)){
+            System.out.println("Title verification passed");
+        }else{
+            System.out.println("Title verification failed");
+        }
 
 
 
