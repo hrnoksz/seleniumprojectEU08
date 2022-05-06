a8eee3eb8c69cb85525057762308e50ed36ff19d
hrnoksz <harun1973@yahoo.com>
2022-05-06 21:56:14 +0300

    day2 T8_BackAndForthNavigation

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T8_BackAndForthNavigation.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T8_BackAndForthNavigation.java
@@ -1,4 +1,28 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T8_BackAndForthNavigation {
+
+    public static void main(String[] args) {
+
+        //TC #8: Back and forth navigation
+        //1- Open a chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2- Go to: https://google.com
+        //3- Click to Gmail from top right.
+        //4- Verify title contains:
+        //Expected: Gmail
+        //5- Go back to Google by using the .back();
+        //6- Verify title equals:
+        //Expected: Google
+
+
+
+    }
 }
