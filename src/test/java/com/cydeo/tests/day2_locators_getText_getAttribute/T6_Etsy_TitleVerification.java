80a72dab8166cde842ec79227fd89022ef16644c
hrnoksz <harun1973@yahoo.com>
2022-05-06 21:25:46 +0300

    day2 T6_Etsy_TitleVerification

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T6_Etsy_TitleVerification.java
@@ -0,0 +1,37 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T6_Etsy_TitleVerification {
+
+    public static void main(String[] args) {
+
+        //TC #1: Etsy Title Verification
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Go to https://www.etsy.com
+        driver.get("https://www.etsy.com");
+
+        //3. Search for “wooden spoon”
+        WebElement etsySearchBox = driver.findElement(By.id("global-enhancements-search-query"));
+        etsySearchBox.sendKeys("wooden spoon" + Keys.ENTER);
+
+        //4. Verify title:
+        //Expected: “Wooden spoon | Etsy”
+        String expectedTitle = "Wooden spoon | Etsy";
+        String actualTitle = driver.getTitle();
+        if(actualTitle.startsWith(expectedTitle)){
+            System.out.println("Title verification is passed");
+        }else{
+            System.out.println("Title verification is failed");
+        }
+    }
+}
