9491aed08f4881009a04f5a2615babdf7d4bb9bf
hrnoksz <harun1973@yahoo.com>
2022-05-07 19:26:21 +0300

    Task2 is done

--- src/test/java/com/cydeo/reviewWithOscar/week02/Task2.java
+++ src/test/java/com/cydeo/reviewWithOscar/week02/Task2.java
@@ -1,5 +1,11 @@
 package com.cydeo.reviewWithOscar.week02;
 
+import com.cydeo.utilities.HandleWait;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class Task2 {
 
     public static void main(String[] args) {
@@ -12,6 +18,36 @@ public class Task2 {
         //- Enter password: test
         //- Click “Sign In” button
         //- Verify title equals: String expectedHomePageTitle = "Web Orders"
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+
+        //title verification before log in
+        String expectedTitleBeforeLogin = "Web Orders Login";
+        if(expectedTitleBeforeLogin.equals(driver.getTitle())){
+            System.out.println("success");
+        }else{
+            System.exit(-1);
+        }
+
+        WebElement usernameBox = driver.findElement(By.id("ctl100_MainContent_username"));
+        WebElement passwordBox = driver.findElement(By.id("ctl100_MainContent_password"));
+        WebElement signInButton = driver.findElement(By.id("ctl100_MainContent_login_button"));
+
+        usernameBox.sendKeys("Tester");
+        passwordBox.sendKeys("test");
+        signInButton.click();
+
+        String expectedHomePageTitle = "Web Orders";
+        if(expectedHomePageTitle.equals(driver.getTitle())){
+            System.out.println("passed");
+        }else{
+            System.out.println("failed");
+        }
+
+
+        HandleWait.staticWait(2);
+        driver.close();
 
     }
 }
