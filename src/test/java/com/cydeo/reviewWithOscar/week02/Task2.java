f5ccc04264ff6fb08bf61e1df61fd1551709c921
hrnoksz <harun1973@yahoo.com>
2022-05-07 19:08:32 +0300

    Task2

---
+++ src/test/java/com/cydeo/reviewWithOscar/week02/Task2.java
@@ -0,0 +1,17 @@
+package com.cydeo.reviewWithOscar.week02;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+
+        //Basic login authentication
+        //- Go to:  http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+        //- Verify title equals: String expectedTitle = "Web Orders Login"
+        //- Enter username: Tester
+        //- Enter password: test
+        //- Click “Sign In” button
+        //- Verify title equals: String expectedHomePageTitle = "Web Orders"
+
+    }
+}
