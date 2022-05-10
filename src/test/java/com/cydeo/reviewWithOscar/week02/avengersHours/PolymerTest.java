28d11522fcc60186ad098eedb18e37491d9abc3e
hrnoksz <harun1973@yahoo.com>
2022-05-10 21:30:36 +0300

    Polymer

--- src/test/java/com/cydeo/reviewWithOscar/week02/avengersHours/PolymerTest.java
+++ src/test/java/com/cydeo/reviewWithOscar/week02/avengersHours/PolymerTest.java
@@ -1,7 +1,11 @@
 package com.cydeo.reviewWithOscar.week02.avengersHours;
 
+import com.cydeo.utilities.HandleWait;
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PolymerTest {
     /*
@@ -22,10 +26,42 @@ public class PolymerTest {
         // driver instance initiates a 'session', until we say driver.close or driver.quit
 
         WebDriver driver = WebDriverFactory.getDriver("Chrome"); //opens an empty Chrome browser
+
+        //maximize page
         driver.manage().window().maximize();
 
         // our driver object bring us many useful methods from the library
         driver.get("http://todomvc.com/");
+
+        //click within the JavaScript tab
+
+        WebElement JSElement = driver.findElement(By.xpath("//div[.='JavaScript']"));
+
+        // why do we locate a web element? to do an action on it
+        JSElement.click();
+
+        //select the Polymer link
+        WebElement polymerLink = driver.findElement(By.linkText("Polymer"));
+        polymerLink.click();
+
+        //The second step should be: Add one Todo item
+        String toDoItem = "This Todo Item is added by user";
+
+        HandleWait.staticWait(1);
+        WebElement toDoBox = driver.findElement(By.id("new-todo"));
+        toDoBox.sendKeys(toDoItem+ Keys.ENTER);
+
+        //to locate the web element for item we entered: //label[.='customText']
+
+        //String locator = "//label[.='customText']"; //I want to use text I am sending as String variable
+
+        String locator = "//label[.='"+toDoItem+"']";
+        HandleWait.staticWait(1);
+        WebElement customToDoItem = driver.findElement(By.xpath(locator));
+
+        if(customToDoItem.isDisplayed()){
+            System.out.println("We added our toDo item successfully");
+        }
 
 
     }
