6ddd18ef4a701d74ff7451affd190855e0dfe1c5
hrnoksz <harun1973@yahoo.com>
2022-05-12 16:12:55 +0300

    TestNG_Selenium

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
@@ -0,0 +1,32 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestNG_Selenium {
+
+    @Test
+    public void selenium_test(){
+        //Do browser driver setup
+        //Open browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        //Maximize the page
+        driver.manage().window().maximize();
+
+        //Implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Get "https://www.google.com
+        driver.get("https://www.google.com");
+
+        //Assert: title is "Google"
+
+
+    }
+
+
+}
