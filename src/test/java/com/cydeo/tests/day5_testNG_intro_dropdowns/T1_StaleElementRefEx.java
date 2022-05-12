6a4c179fd00155a92a15d275503b374abe0022fd
hrnoksz <harun1973@yahoo.com>
2022-05-12 13:48:25 +0300

    Task2 is done

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
@@ -41,6 +41,7 @@ public class T1_StaleElementRefEx {
             System.out.println("-->This means the web element is completely deleted from the page");
             System.out.println("deleteButton.isDisplayed()=false");
         }
+        driver.close();
 
 
         //USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
