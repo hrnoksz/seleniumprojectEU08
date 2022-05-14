0895d4378d75196fe8c87b643b8566ff5e366cd5
hrnoksz <harun1973@yahoo.com>
2022-05-14 23:06:30 +0300

    WebOrderTest is done

--- src/test/java/com/cydeo/reviewWithOscar/week03/TestCase_02.java
+++ src/test/java/com/cydeo/reviewWithOscar/week03/TestCase_02.java
@@ -21,30 +21,33 @@ public class TestCase_02 {
         driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
 
         //2.    Login with-----Username: Tester, password: test
-       WebElement usernameInput = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_username']"));
-       usernameInput.sendKeys("Tester");
+        WebElement usernameInput = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_username']"));
+        usernameInput.sendKeys("Tester");
 
-       WebElement passwordInput = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_password']"));
-       passwordInput.sendKeys("test");
+        WebElement passwordInput = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_password']"));
+        passwordInput.sendKeys("test");
 
-       WebElement loginBtn = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_login_button']"));
-       loginBtn.click();
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_login_button']"));
+        loginBtn.click();
 
         //3.    Select one of the checkbox and delete one person
-       WebElement checkboxBtn = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_orderGrid_ctl02_OrderSelector']"));
-       checkboxBtn.click();
+        WebElement checkboxBtn = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_orderGrid_ctl02_OrderSelector']"));
+        checkboxBtn.click();
 
         WebElement deleteBtn = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_btnDelete']"));
         deleteBtn.click();
 
         //4.    Then verify that deleted item is no longer exists
-        List<WebElement> list = driver.findElements(By.xpath("//input[@id='ctl00_MainContent_orderGrid_ctl02_OrderSelector']"));
-        for (WebElement each : list) {
-            if(!each.equals("Paul Brown")){
-                System.out.println("Deleted item verification passed");
-            }else{
-                System.out.println("failed");
-            }
+
+        List<WebElement> lists = driver.findElements(By.xpath("//input[@id='ctl00_MainContent_orderGrid_ctl02_OrderSelector']"));
+
+        for (WebElement each : lists) {
+            System.out.println(each);
         }
     }
 }
+
+
+
+
+
