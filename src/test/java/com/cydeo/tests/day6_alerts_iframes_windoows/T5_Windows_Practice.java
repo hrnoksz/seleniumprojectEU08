2c616eacc8f6b1f13c2a30ba80e3029318937c75
hrnoksz <harun1973@yahoo.com>
2022-05-19 19:44:53 +0300

    Windows practice is done

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windoows/T5_Windows_Practice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windoows/T5_Windows_Practice.java
@@ -61,6 +61,9 @@ public class T5_Windows_Practice {
 
         System.out.println("Title after click: " + actualTitle);
 
+        //If we want to go back to main page, we can use already stored main handle
+        //driver.switchTo().window(mainHandle);
+
     }
     @AfterMethod
     public void teardownMethod(){
