81530e9a841f54190e3a3ef9ba7d5575c681688a
hrnoksz <harun1973@yahoo.com>
2022-05-19 18:06:21 +0300

    Iframe practice is done

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windoows/T4_IframePractice.java
@@ -0,0 +1,63 @@
+package com.cydeo.tests.day6_alerts_iframes_windoows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T4_IframePractice {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/iframe");
+    }
+    //@Ignore function is test will not be executed until get rid of @Ignore
+    @Test
+    public void iframe_test(){
+        //We need to switch driver's focus to iframe
+        //option #1- switching to iframe using id attribute value!!!!!!!!!!!!!!!!!!!!!!!
+        //driver.switchTo().frame("mce_0_ifr");
+
+        //option #2- passing index number of iframe
+        //driver.switchTo().frame(0);
+
+        //option #3- locate as a web element and pass in frame() method
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']")));
+
+        //Locate the p tag
+        WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
+
+        Assert.assertTrue(yourContentGoesHereText.isDisplayed());
+
+        //Verify “An iFrame containing the TinyMCE WYSIWYG Editor”
+        //To be able to verify the header, we must switch back to "main HTML"
+        driver.switchTo().parentFrame();
+
+        WebElement headerText = driver.findElement(By.xpath("//h3"));
+
+        // assertion of header text is displayed or not
+        Assert.assertTrue(headerText.isDisplayed());
+
+
+    }
+
+
+
+}
+/*
+TC #4: Iframe practice
+1. Create a new class called: T4_Iframes
+2. Create new test and make set ups
+3. Go to: https://practice.cydeo.com/iframe
+4. Assert: “Your content goes here.” Text is displayed.
+5. Assert:
+ */
