4b79bc50989bd367e4a2c5406252759d30ad89fe
hrnoksz <harun1973@yahoo.com>
2022-05-21 22:43:15 +0300

    Window handle task is done

--- src/test/java/com/cydeo/reviewWithOscar/week04/WindowHandleTask.java
+++ src/test/java/com/cydeo/reviewWithOscar/week04/WindowHandleTask.java
@@ -14,8 +14,9 @@ public class WindowHandleTask {
 
     /*
     automate the following scenarios for this website https://shino.de/parkcalc/
-Scenario 1)
--  Using the Valet parking lot, set today as the entry date & time and tomorrow as the exit giving a full 24 hour window (you can attempt to use the DatePicker).
+    Scenario 1)
+    -  Using the Valet parking lot, set today as the entry date & time and
+    tomorrow as the exit giving a full 24 hour window (you can attempt to use the DatePicker).
      */
 
     /*
