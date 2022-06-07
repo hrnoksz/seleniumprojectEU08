96707d774807621866ccf3e7d9fa3249284c8f8e
hrnoksz <harun1973@yahoo.com>
2022-06-07 10:23:11 +0300

    task_4_and_task_5_test are done

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -19,7 +19,7 @@ public class Driver {
     We make WebDriver private, because we want to close access from outside the class
     We make it static because we will use it in a static method
      */
-    private static WebDriver driver;
+    private static WebDriver driver; //value is null by default
 
     /*
     Create a re-usable utility method which will return same driver instance when we call it
@@ -28,8 +28,9 @@ public class Driver {
         if(driver == null){
             /*
             We read our browserType from configuration.properties.
-            This way, we can control which browser is opened from outside our code, from configuration.properties.
-             */
+            This way, we can control which browser is opened from outside our code,
+            from configuration.properties.
+            */
             String browserType = ConfigurationReader.getProperty("browser");
 
             /*
@@ -53,6 +54,15 @@ public class Driver {
 
         }
         return driver;
+    }
+    /*
+    This method will make sure our driver value is always null after using quit() method
+     */
+    public static void closeDriver(){
+        if(driver != null){
+            driver.quit(); //this line will terminate the existing session. Value will not even be null
+            driver = null;
+        }
     }
 
 }
